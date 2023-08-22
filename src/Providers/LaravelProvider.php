6be4df18e6307d18d4be3db12d5ffd357808e326
Ivan Litovchenko <iv-litovchenko@mail.ru>
2023-08-22 14:49:33 +0300

    Update LaravelProvider.php

--- src/Providers/LaravelProvider.php
+++ src/Providers/LaravelProvider.php
@@ -51,10 +51,10 @@ final class LaravelProvider extends ServiceProvider
             ]);
         }
 
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'migrationassistant');
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'migrationassistant'); // return view('migrationassistant::dashboard');
+        // $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        // $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        // $this->loadTranslationsFrom(__DIR__ . '/../lang', 'migrationassistant');
+        // $this->loadViewsFrom(__DIR__ . '/../resources/views', 'migrationassistant'); // return view('migrationassistant::dashboard');
 
         // $this->publishes([
         //     __DIR__.'/../resources/views' => resource_path('views/vendor/courier'),
